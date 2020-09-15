2f85e589a95e0ddc4f6634eb8835bf885908899b
RidgeA <RidgeA@users.noreply.github.com>
2020-09-15 12:39:33 +0300

    Revert "Solution for 01-intro/01-sum"

--- 01-intro/01-sum/sum.js
+++ 01-intro/01-sum/sum.js
@@ -1,9 +1,5 @@
 function sum(a, b) {
   /* ваш код */
-  if (typeof(a) !== 'number' || typeof(b) !== 'number') {
-    throw new TypeError('Both arguments should be numbers.');
-  }
-  return a + b;
 }
 
 module.exports = sum;
